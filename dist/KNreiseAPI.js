a0e27153f85334dc2aa069a43fbfb2e38fbdc01e
Atle Frenvik Sveen <atle.sveen@norkart.no>
2015-05-22 13:01:13 +0200

    update version

--- dist/KNreiseAPI.js
+++ dist/KNreiseAPI.js
@@ -822,18 +822,19 @@ KR.WikipediaAPI = function () {
     };
 };
 
+/*global toGeoJSON: false */
 var KR = this.KR || {};
 
 KR.UtnoAPI = function () {
     'use strict';
 
     function getData(dataset, callback, errorCallback) {
-
         if (dataset.type === 'gpx') {
             var url = 'http://ut.no/tur/' + dataset.id + '/gpx/';
-            KR.Util.sendRequest(url, toGeoJSON.gpx, callback);
+            KR.Util.sendRequest(url, toGeoJSON.gpx, callback, errorCallback);
+        } else {
+            errorCallback('unknown type');
         }
-
     }
 
     return {

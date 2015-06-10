940c1acddca5a1f39971444a2d26bf46f3ae1829
Atle Frenvik Sveen <atle.sveen@norkart.no>
2015-06-10 10:50:19 +0200

    get images, tag search

--- dist/KNreiseAPI.js
+++ dist/KNreiseAPI.js
@@ -945,12 +945,21 @@ var KR = this.KR || {};
 KR.FlickrAPI = function (apikey) {
     'use strict';
 
-    var BASE_URL = 'http://crossorigin.me/https://api.flickr.com/services/rest/';
+    var BASE_URL = 'https://api.flickr.com/services/rest/';
+
+    var imageTemplate = _.template('https://farm<%= farm %>.staticflickr.com/<%= server %>/<%= id %>_<%= secret %>_<%= size %>.jpg');
+
+    function getImageUrl(photo, size) {
+        return imageTemplate(_.extend({size: size}, photo));
+    }
 
     function _parser(response) {
-        response = JSON.parse(response);
         var features = _.map(response.photos.photo, function (item) {
             var properties = KR.Util.dictWithout(item, 'latitude', 'longitude');
+
+            //see https://www.flickr.com/services/api/misc.urls.html for sizes
+            properties.thumbnail = getImageUrl(item, 's');
+            properties.image = getImageUrl(item, 'z');
             return KR.Util.createGeoJSONFeature(
                 {
                     lat: parseFloat(item.latitude),
@@ -963,21 +972,28 @@ KR.FlickrAPI = function (apikey) {
     }
 
     function getBbox(dataset, bbox, callback, errorCallback) {
+
+        if (!_.has(dataset, 'user_id')) {
+            KR.Util.handleError(errorCallback, 'must specify user_id');
+            return;
+        }
+
         var params = {
             method: 'flickr.photos.search',
             user_id: dataset.user_id,
             api_key: apikey,
             bbox: bbox,
             has_geo: true,
-            extras: 'geo',
+            extras: 'geo,tags',
             format: 'json',
-            nojsoncallback: 1,
-            pr_page: 10 //500
+            nojsoncallback: 1
         };
-        if (dataset.query) {
-            params.where = dataset.query;
+
+        if (_.has(dataset, 'tags')) {
+            params.tags = dataset.tags.join(',');
+            params.tag_mode = dataset.tag_mode || 'all';
         }
-        var layer = dataset.layer;
+        
         var url = BASE_URL + '?' + KR.Util.createQueryParameterString(params);
         KR.Util.sendRequest(url, _parser, callback, errorCallback);
     }

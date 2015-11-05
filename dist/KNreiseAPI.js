ed66f31e295f47d827e23ba4d3fd14e1daa0516b
Atle Frenvik Sveen <atle.sveen@norkart.no>
2015-11-05 12:10:43 +0100

    add support for flickr groups

--- dist/KNreiseAPI.js
+++ dist/KNreiseAPI.js
@@ -1514,21 +1514,28 @@ KR.FlickrAPI = function (apiName, options) {
             KR.Util.handleError(errorCallback, response.message, response);
             return;
         }
-        var features = _.map(response.photos.photo, function (item) {
-            var properties = KR.Util.dictWithout(item, 'latitude', 'longitude');
+        var features = _.chain(response.photos.photo)
+            .filter(function (item) {
+                var lat = parseFloat(item.latitude);
+                var lng = parseFloat(item.longitude);
+                return (lat != 0 || lng != 0);
+            })
+            .map(function (item) {
+                var properties = KR.Util.dictWithout(item, 'latitude', 'longitude');
 
-            //see https://www.flickr.com/services/api/misc.urls.html for sizes
-            properties.thumbnail = getImageUrl(item, 's');
-            properties.image = getImageUrl(item, 'z');
-            return KR.Util.createGeoJSONFeature(
-                {
-                    lat: parseFloat(item.latitude),
-                    lng: parseFloat(item.longitude)
-                },
-                properties,
-                apiName + '_' + item.id
-            );
-        });
+                //see https://www.flickr.com/services/api/misc.urls.html for sizes
+                properties.thumbnail = getImageUrl(item, 's');
+                properties.image = getImageUrl(item, 'z');
+                return KR.Util.createGeoJSONFeature(
+                    {
+                        lat: parseFloat(item.latitude),
+                        lng: parseFloat(item.longitude)
+                    },
+                    properties,
+                    apiName + '_' + item.id
+                );
+            })
+            .value();
         return KR.Util.createFeatureCollection(features);
     }
 
@@ -1547,7 +1554,9 @@ KR.FlickrAPI = function (apiName, options) {
                 params.page = response.photos.page + 1;
                 _sendRequest(params)
             } else {
-                callback(KR.Util.createFeatureCollection(result));
+                var fc = KR.Util.createFeatureCollection(result);
+                console.log(fc);
+                callback(fc);
             }
         }
 
--- src/flickr.js
+++ src/flickr.js
@@ -19,21 +19,28 @@ KR.FlickrAPI = function (apiName, options) {
             KR.Util.handleError(errorCallback, response.message, response);
             return;
         }
-        var features = _.map(response.photos.photo, function (item) {
-            var properties = KR.Util.dictWithout(item, 'latitude', 'longitude');
+        var features = _.chain(response.photos.photo)
+            .filter(function (item) {
+                var lat = parseFloat(item.latitude);
+                var lng = parseFloat(item.longitude);
+                return (lat != 0 || lng != 0);
+            })
+            .map(function (item) {
+                var properties = KR.Util.dictWithout(item, 'latitude', 'longitude');
 
-            //see https://www.flickr.com/services/api/misc.urls.html for sizes
-            properties.thumbnail = getImageUrl(item, 's');
-            properties.image = getImageUrl(item, 'z');
-            return KR.Util.createGeoJSONFeature(
-                {
-                    lat: parseFloat(item.latitude),
-                    lng: parseFloat(item.longitude)
-                },
-                properties,
-                apiName + '_' + item.id
-            );
-        });
+                //see https://www.flickr.com/services/api/misc.urls.html for sizes
+                properties.thumbnail = getImageUrl(item, 's');
+                properties.image = getImageUrl(item, 'z');
+                return KR.Util.createGeoJSONFeature(
+                    {
+                        lat: parseFloat(item.latitude),
+                        lng: parseFloat(item.longitude)
+                    },
+                    properties,
+                    apiName + '_' + item.id
+                );
+            })
+            .value();
         return KR.Util.createFeatureCollection(features);
     }
 
@@ -52,7 +59,8 @@ KR.FlickrAPI = function (apiName, options) {
                 params.page = response.photos.page + 1;
                 _sendRequest(params)
             } else {
-                callback(KR.Util.createFeatureCollection(result));
+                var fc = KR.Util.createFeatureCollection(result);
+                callback(fc);
             }
         }
 
